5ce2a97008c6f02f59c154be826847494bb6c542
xuld <xuld@vip.qq.com>
2016-01-13 14:46:03 +0800

    更新项目

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Coders' Girl Friend
 ===================================
 
 Coders' Girl Friend can never be installed properly. T_T
+
+	npm install girl-friend
+
+	
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("-------------------------------------------------------");
+console.error("\033[49;31;1mCannot find reference 'House'\033[0m");
+console.error("\033[49;31;1mCannot find reference 'Car'\033[0m");
+console.log("-------------------------------------------------------");
+process.exit(1);
